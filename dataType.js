7e8e7e989ba36d4ef692bc13ff990b8c76be8832
Abi22Git <abiramiism.93@gmail.com>
2025-12-17 18:17:11 +0530

    Add files via upload

---
+++ dataType.js
@@ -0,0 +1,12 @@
+//console.log("welcome")
+let firstName ="Abi"
+let companyName = 'Indium'
+let mobileNumber = 123
+let isAutomation = false
+let hasPlaywright = null
+
+console.log("This is my mobile number :" + mobileNumber)
+console.log("My company name :"+ companyName  + " Is datatype is : " + typeof companyName)
+console.log("There is no value for this :" + hasPlaywright + " and its datatype is: " + typeof hasPlaywright)
+ 
+
---
+++ ifCondition.js
@@ -0,0 +1,18 @@
+
+let mark =100
+//mark>=90 -> A grade
+//mark>=80 -> B grade
+//mark>=70 -> C grade
+
+if (mark >=70) 
+{
+    console.log("Youre in A grade");
+} 
+else if (mark <=10)
+{
+    console.log("youre fail")
+}
+else 
+{
+    console.log("Failed")
+}
