84d6fe5b775cc2cd378544c97a1aeab3ab66845c
Abi22Git <abiramiism.93@gmail.com>
2025-12-18 18:50:12 +0530

    Add files via upload

---
+++ Switch.js
@@ -0,0 +1,31 @@
+//*ClassRoom(SwitchCase):
+//let day =4
+//case 1-sunday
+//case 2-monday
+//case 3-tuesday     
+//case 4-wednesday
+//case 5-thursday
+//default *//
+
+let weekday = 6
+switch(weekday)
+{
+    case 1:
+    console.log("Today is Monday")
+    break
+    case 2:
+    console.log("Today is tuesday")
+    break
+    case 3:
+    console.log("Today is wednesday")
+    break
+    case 4:
+    console.log("TOday is thursday")
+    break
+    case 5:
+    console.log("Today is Friday")
+    break
+    default: 
+    console.log("Its not a weekday")
+    }
+
